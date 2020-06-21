b91deacffbdc330a425e6b8e8e99b1d27a178fb5
Mor Paz <1912.mor@gmail.com>
2020-06-21 23:04:48 +0300

    update immediate addition command in assembler

--- test.s
+++ test.s
@@ -3,7 +3,7 @@ main:   # Put your code here
         # Loads the value from memory in 8 bit offset, to register t1
         lw              t1, 8(x0)
         # Perform add i to value loaded from memory, adding 12 and XORing
-        addi            t2, t1, 0xC
+        addi            t2, t1, 12
         # Storing the result in memory at offset 16
         sw              t2, 16(x0)
         add		t6, x0, x0
